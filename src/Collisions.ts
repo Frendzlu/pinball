5a4d2365cb99c0b4a739ecdbc5b250f71f7e8e84
Frendzlu <mateuszfrancik.mtf@gmail.com>
2022-05-23 23:36:15 +0200

    fixed directory layout #2

---
+++ src/Collisions.ts
@@ -0,0 +1,6 @@
+export enum CollisionConditions {
+    Above,
+    Right,
+    Below,
+    Left
+}
---
+++ src/main.ts
@@ -0,0 +1,19 @@
+import "./style.css"
+
+import SixteenSegmentDisplay from "./SixteenSegmentDisplay/SixteenSegmentDisplay";
+import {GameArea} from "./GameArea/GameArea";
+import {Envs} from "./envs";
+
+let display = new SixteenSegmentDisplay("display", "test")
+display.clearText()
+display.renderText("!%*()-_=+\\|{}[]'\"`.,?/")
+
+let x = new GameArea("ga", "gac")
+Envs.debugMode = true
+Envs.preferredDarkMode = true
+Envs.showBounceChecks = true
+var envs = Envs
+console.log(envs)
+display.clearText()
+display.renderText("Player 1ś")
+x.render()
